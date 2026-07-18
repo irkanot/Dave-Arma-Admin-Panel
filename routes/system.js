89a194f27cc0d5665759e8b1c39781b011797c5d
irkanot <irkanot@users.noreply.github.com>
2026-07-18 10:09:06 +0200

    Fix Windows host metrics collection

--- routes/system.js
+++ routes/system.js
@@ -5,7 +5,11 @@ const getSystemMetrics = require('../lib/system-metrics')
 module.exports = function (installRoot, accessControl) {
   const router = express.Router()
   router.get('/', accessControl.requirePermission(permissions.servers.view), function (req, res) {
-    res.json(getSystemMetrics(installRoot))
+    try {
+      res.json(getSystemMetrics(installRoot))
+    } catch (err) {
+      res.status(500).json({ error: 'Unable to collect server metrics: ' + err.message })
+    }
   })
   return router
 }
